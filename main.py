7defebc4f31851a11acafa349b4ddb1dc3ca0512
anton <unton83@yandex.ru>
2024-03-16 16:44:01 +0300

    Refactoring

--- main.py
+++ main.py
@@ -2,27 +2,37 @@ from reportlab.lib.units import mm
 from reportlab.pdfgen.canvas import Canvas
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
-from types import MappingProxyType
 import datetime as dt
 from copy import deepcopy
 import locale
 
-
+# Need to set locale
 locale.setlocale(locale.LC_ALL, "")
+
+# Set font
 pdfmetrics.registerFont(TTFont('Arial', 'arial.ttf'))
+
+# Set page size
 page_w, page_h = (212*mm, 299*mm)
-pagesize = (page_w, page_h)
+PAGE_SIZE = (page_w, page_h)
 
-borders_left = 5 * mm
-borders_right = 5 * mm
-borders_top = 5 * mm
-borders_bottom = 5 * mm
+# Set page borders
+borders_left_default = 5 * mm
+borders_right_default = 5 * mm
+borders_top_default = 5 * mm
+borders_bottom_default = 5 * mm
+
+# Set colors
+WORKDAY_HIGHLIGHT = (0.988, 0.882, 0.541)
+WEEKEND_HIGHLIGHT = (0.9, 0.7, 0.6)
+TEXT_COLOR_FILL_GRAY = 0.3
+LINE_COLOR_GRAY = 0.8
 
 
-def liner_horizontal(pagesize,  # num_of_lines: int = 33,
+def liner_horizontal(pagesize: tuple,  # num_of_lines: int = 33,
                      increment,
-                     borders_left=borders_left, borders_right=borders_right,
-                     borders_top=borders_top, borders_bottom=borders_bottom):
+                     borders_left=borders_left_default, borders_right=borders_right_default,
+                     borders_top=borders_top_default, borders_bottom=borders_bottom_default):
     x1 = borders_left
     x2 = pagesize[0] - borders_right
     y = borders_bottom
@@ -30,23 +40,14 @@ def liner_horizontal(pagesize,  # num_of_lines: int = 33,
     while y <= pagesize[1] - borders_top:
         coords_list.append((x1, y, x2, y))
         y += increment
-    # page_height = pagesize[1] - borders_top
-    # draw_height = pagesize[1] - borders_top - borders_bottom
-    # for line in range(num_of_lines-1):
-    #     coords_list.append((x1, y, x2, y))
-    #     y += draw_height / (num_of_lines - 1)
-    # coords_list.append((x1, pagesize[1] - borders_top, x2, pagesize[1] - borders_top))
-    # print(y)
-    # for y in range(int(borders['bottom']), page_height):
-    #     if y / (page_h // num_of_lines) == y // (page_h // num_of_lines):
-    #         coords_list.append((x1, y, x2, y))
+
     return coords_list
 
 
-def liner_vertical(pagesize,  # num_of_lines: int = 7,
+def liner_vertical(pagesize: tuple,  # num_of_lines: int = 7,
                    increment,
-                   borders_left=borders_left, borders_right=borders_right,
-                   borders_top=borders_top, borders_bottom=borders_bottom):
+                   borders_left=borders_left_default, borders_right=borders_right_default,
+                   borders_top=borders_top_default, borders_bottom=borders_bottom_default):
     y1 = borders_bottom
     y2 = pagesize[1] - borders_top
     x = borders_left
@@ -54,11 +55,7 @@ def liner_vertical(pagesize,  # num_of_lines: int = 7,
     while x <= pagesize[0]:
         coords_list.append((x, y1, x, y2))
         x += increment
-    # draw_width = pagesize[0] - borders_right - borders_left
-    # for line in range(num_of_lines-1):
-    #     coords_list.append((x, y1, x, y2))
-    #     x += draw_width / (num_of_lines-1)
-    # coords_list.append((pagesize[0] - borders_right, y1, pagesize[0] - borders_right, y2))
+
     return coords_list
 
 
@@ -83,34 +80,29 @@ def month_by_weeks(year=2024, month=1) -> list[list]:
         last_day = day
     if last_day.weekday() != 6:
         weeks.append(week)
-    # for day in dates:
-    #     print(day.strftime('%d, %a'))
+
     return weeks
 
 
 def planer_month_weeks(canvas: Canvas, weeks: list[list[dt.date]]):
     borders_top = 12 * mm
-    draw_height = pagesize[1] - borders_top - borders_bottom
+    draw_height = PAGE_SIZE[1] - borders_top - borders_bottom_default
     increment_v = draw_height / (33 - 1)
-    lines = liner_horizontal(pagesize, increment_v, borders_top=borders_top)
+    lines = liner_horizontal(PAGE_SIZE, increment_v, borders_top=borders_top)
     for week in weeks:
         for n, line in enumerate(lines[::-1], start=1):
             if n in [1, 9, 15, 21, 27]:
                 day = week.pop(0)
                 if day is not None:
                     day_string = day.strftime('%d, %a')
-                    canvas.setFillColorRGB(0.988, 0.882, 0.541)
+                    canvas.setFillColorRGB(*WORKDAY_HIGHLIGHT)
                     canvas.rect(line[0], line[1], 20 * mm, 7 * mm, stroke=0, fill=1)
                     canvas.setFont('Arial', 16)
-                    canvas.setFillGray(0.3)
+                    canvas.setFillGray(TEXT_COLOR_FILL_GRAY)
                     canvas.drawString(line[0] + 1 * mm, line[1] + 1.5 * mm, day_string)
-        canvas.setStrokeGray(0.8)
+        canvas.setStrokeGray(LINE_COLOR_GRAY)
         canvas.lines(lines)
         canvas.showPage()
-    # canvas.setStrokeGray(0.8)
-    # canvas.lines(lines)
-    # canvas.showPage()
-    # canvas.save()
 
 
 def planer_month_whole(canvas: Canvas, weeks: list[list[dt.date]], month_name):
@@ -118,15 +110,15 @@ def planer_month_whole(canvas: Canvas, weeks: list[list[dt.date]], month_name):
     borders_right = 5 * mm
     borders_top = 20 * mm
     borders_bottom = 40 * mm
-    canvas.setFillGray(0.3)
+    canvas.setFillGray(TEXT_COLOR_FILL_GRAY)
     canvas.setFont('Arial', 20)
-    canvas.drawString(borders_left, pagesize[1] - 15 * mm, month_name)
-    draw_width = pagesize[0] - borders_right - borders_left
-    draw_height = pagesize[1] - borders_top - borders_bottom
+    canvas.drawString(borders_left, PAGE_SIZE[1] - 15 * mm, month_name)
+    draw_width = PAGE_SIZE[0] - borders_right - borders_left
+    draw_height = PAGE_SIZE[1] - borders_top - borders_bottom
     increment_v = draw_height / (6 - 1)
     increment_h = draw_width / (8 - 1)
-    lines_h = liner_horizontal(pagesize, increment_v, borders_top=borders_top, borders_bottom=borders_bottom)
-    lines_v = liner_vertical(pagesize, increment_h, borders_top=borders_top, borders_bottom=borders_bottom)
+    lines_h = liner_horizontal(PAGE_SIZE, increment_v, borders_top=borders_top, borders_bottom=borders_bottom)
+    lines_v = liner_vertical(PAGE_SIZE, increment_h, borders_top=borders_top, borders_bottom=borders_bottom)
     for wom, week in enumerate(weeks):
         # for n, line in enumerate(lines_h[::-1], start=1):
         for line_v in lines_v[:-1]:
@@ -135,13 +127,13 @@ def planer_month_whole(canvas: Canvas, weeks: list[list[dt.date]], month_name):
                 day_string = day.strftime('%d, %a')
                 canvas.setFont('Arial', 16)
                 if day.weekday() in [5, 6]:
-                    canvas.setFillColorRGB(0.9, 0.7, 0.6)
+                    canvas.setFillColorRGB(*WEEKEND_HIGHLIGHT)
                 else:
-                    canvas.setFillColorRGB(0.988, 0.882, 0.541)
+                    canvas.setFillColorRGB(*WORKDAY_HIGHLIGHT)
                 canvas.rect(line_v[0], line_v[3] - 7 * mm - increment_v * wom, increment_h, 7 * mm, stroke=0, fill=1)
-                canvas.setFillGray(0.3)
+                canvas.setFillGray(TEXT_COLOR_FILL_GRAY)
                 canvas.drawString(line_v[0] + 1 * mm, line_v[3] - 7 * mm - increment_v * wom + 1.5 * mm, day_string)
-    canvas.setStrokeGray(0.8)
+    canvas.setStrokeGray(LINE_COLOR_GRAY)
     canvas.lines(lines_h)
     canvas.lines(lines_v)
     canvas.showPage()
@@ -149,27 +141,34 @@ def planer_month_whole(canvas: Canvas, weeks: list[list[dt.date]], month_name):
 
 def planer_blank_pages(canvas, pages=5):
     borders_top = 12 * mm
-    draw_height = pagesize[1] - borders_top - borders_bottom
+    draw_height = PAGE_SIZE[1] - borders_top - borders_bottom_default
     increment_v = draw_height / (33 - 1)
-    lines = liner_horizontal(pagesize, increment_v, borders_top=borders_top)
+    lines = liner_horizontal(PAGE_SIZE, increment_v, borders_top=borders_top)
     for _ in range(pages):
-        canvas.setStrokeGray(0.8)
+        canvas.setStrokeGray(LINE_COLOR_GRAY)
         canvas.lines(lines)
-        canvas.setFillColorRGB(0.988, 0.882, 0.541)
+        canvas.setFillColorRGB(*WORKDAY_HIGHLIGHT)
         canvas.rect(lines[-1][0], lines[-1][1], 110 * mm, 7 * mm, stroke=0, fill=1)
         canvas.showPage()
 
 
 def planer_month(year=2024, month=1):
     filename = f'{year}_{month:02}.pdf'
-    canvas = Canvas(filename, pagesize=pagesize)
+    canvas = Canvas(filename, pagesize=PAGE_SIZE)
     month_name = dt.date(year, month, 1).strftime('%B')
     weeks = month_by_weeks(year, month)
+
+    # Adding page for whole month
     planer_month_whole(canvas, deepcopy(weeks), month_name)
+
+    # Adding pages for weeks
     planer_month_weeks(canvas, deepcopy(weeks))
-    planer_blank_pages(canvas)
+
+    # Adding blank pages with lines
+    planer_blank_pages(canvas, pages=5)
+
     canvas.save()
 
 
 if __name__ == '__main__':
-    planer_month(month=4)
+    planer_month(month=5)
